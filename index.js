bfb99ba7dca9687ef009cb8fd2d8307db0ac3b97
Peter <peter@hertigcarl.se>
2023-03-21 23:09:29 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,71 @@
+import puppeteer from 'puppeteer';
+import fs from 'fs';
+import { tidy } from 'htmltidy2'
+import { exec } from 'node:child_process'
+import prettier from 'prettier';
+
+let url = process.argv[2]
+
+var opts = {
+    doctype: 'html5',
+    hideComments: false, //  multi word options can use a hyphen or "camel case"
+    indent: true,
+  }
+
+let interceptedRequestCount = -1
+async function logRequest(interceptedRequest) {
+    interceptedRequestCount++
+
+    if (interceptedRequestCount === 0) {
+        switch(interceptedRequest.status()) {
+            case 200:
+                let text = await interceptedRequest.text()
+                text = prettier.format(text, {parser: 'html'});
+
+                // text = text.split('\n').join('')
+                // let html = await new Promise(resolve => {
+                //     tidy(text, opts, (_err, html) => resolve(html))
+                // })
+                fs.writeFileSync('index-1.html', text, 'utf-8')
+                break;
+            case 301:
+            case 302:
+                console.log(`The site is redirected. Try running the command with the following url: ${interceptedRequest.headers()['location']}`)
+                process.exit(1)
+            default:
+                console.log(`The site did not return success code: ${interceptedRequest.status()}`)
+                process.exit(1)
+        }
+    }
+}
+
+
+(async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+
+    page.on('response', logRequest);
+    await page.goto(url); //, {waitUntil: 'networkidle0'}
+    // Sometime later...
+    page.off('response', logRequest);
+
+    let text = await page.$eval('html', el => el.outerHTML);
+    text = prettier.format(text, {parser: 'html'});
+
+    // text = text.split('\n').join('')
+    
+    // let html = await new Promise(resolve => {
+    //     tidy(text, opts, (_err, html) => resolve(html))
+    // })
+
+    fs.writeFileSync('index-2.html', text, 'utf-8')
+
+    await browser.close();
+
+    const cmd = await new Promise(resolve => exec('diff -u index-1.html index-2.html | npx diff2html -i stdin', resolve));
+
+
+    // npx diff2html -i stdin
+    // diff -u index-1.html index-2.html | npx diff2html -i stdin
+
+})();
