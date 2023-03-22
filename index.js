ae8b61479eb6b0faad6f3f2c7b9966b7beac81db
Peter <peter@hertigcarl.se>
2023-03-22 09:24:52 +0100

    Fixed problem with diff2html

--- index.js
+++ index.js
@@ -5,12 +5,6 @@ import prettier from 'prettier';
 
 let url = process.argv[2]
 
-var opts = {
-    doctype: 'html5',
-    hideComments: false, //  multi word options can use a hyphen or "camel case"
-    indent: true,
-  }
-
 let interceptedRequestCount = -1
 async function logRequest(interceptedRequest) {
     interceptedRequestCount++
@@ -21,10 +15,6 @@ async function logRequest(interceptedRequest) {
                 let text = await interceptedRequest.text()
                 text = prettier.format(text, {parser: 'html'});
 
-                // text = text.split('\n').join('')
-                // let html = await new Promise(resolve => {
-                //     tidy(text, opts, (_err, html) => resolve(html))
-                // })
                 fs.writeFileSync('index-1.html', text, 'utf-8')
                 break;
             case 301:
@@ -38,36 +28,24 @@ async function logRequest(interceptedRequest) {
     }
 }
 
-
 (async () => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
 
     page.on('response', logRequest);
-    await page.goto(url); //, {waitUntil: 'networkidle0'}
-    // Sometime later...
+    await page.goto(url)
     page.off('response', logRequest);
 
     let text = await page.$eval('html', el => el.outerHTML);
     text = prettier.format(text, {parser: 'html'});
 
-    // text = text.split('\n').join('')
-    
-    // let html = await new Promise(resolve => {
-    //     tidy(text, opts, (_err, html) => resolve(html))
-    // })
-
     fs.writeFileSync('index-2.html', text, 'utf-8')
 
     await browser.close();
 
-    const [err, res] = await new Promise(resolve => exec('diff -u index-1.html index-2.html | npx diff2html -i stdin', (x, y) => resolve([x,y])));
+    const [err, res] = await new Promise(resolve => exec('diff -u index-1.html index-2.html | npx diff2html-cli -i stdin', (x, y) => resolve([x,y])));
     
     if(err) {
        console.error(err) 
     }
-
-    // npx diff2html -i stdin
-    // diff -u index-1.html index-2.html | npx diff2html -i stdin
-
 })();
